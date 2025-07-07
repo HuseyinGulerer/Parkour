9ab121e78df1f279e4402a1a6a87fcaa3ecec3da
Hüseyin Gülerer <gulererhuseyin50@gmail.com>
2025-07-07 21:59:31 +0300

    v1.5

--- Assets/Scripts/CameraFallow.cs
+++ Assets/Scripts/CameraFallow.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;  // Takip edilecek karakter
-    public Vector3 offset = new Vector3(0, 100, -115);
+    public Vector3 offset = new Vector3(0, 130, -145);
     public float smoothSpeed = 10f;
 
     void LateUpdate()
--- Assets/Scripts/PanelController.cs
+++ Assets/Scripts/PanelController.cs
@@ -5,6 +5,7 @@ public class PanelController : MonoBehaviour
     public GameObject startPanel;
     public GameObject gameUIandObjects;
     public GameObject menuButton;  // Menü butonu buraya
+    public GameObject finishPanel;
 
     public void CloseStartPanel()
     {
@@ -16,5 +17,10 @@ public class PanelController : MonoBehaviour
 
         if (menuButton != null)
             menuButton.SetActive(true);  // Menü butonunu göster
+    }
+    public void CloseFinishPanel()
+    {
+        if (finishPanel != null)
+            finishPanel.SetActive(false);
     }
 }
